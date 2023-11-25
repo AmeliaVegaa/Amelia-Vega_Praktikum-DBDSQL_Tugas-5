28870e0e27d70c55a39a5b2322be9a6904fe5d7e
AmeliaVegaa <133181467+AmeliaVegaa@users.noreply.github.com>
2023-11-26 00:26:09 +0700

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,3 +1,4 @@
+# 1. FUNGSI SKALAR
 select concat(ID,name) as profil
 from sampel_university.student;
 
@@ -12,3 +13,15 @@ from sampel_university.course;
 
 select name, replace (name,'JONO','BUDI') as NAMA_BARU
 from akademik.mahasiswa ;
+
+
+# 2. FUNGSI AGGREGASI
+select i.dept_name, sum(i.salary) "Salary Sum" from sampel_university.instructor i group by i.dept_name;
+
+select count(c.course_id) "Jumlah course yang ada" from sampel_university.course c;
+
+select i.dept_name, avg(i.salary) 'avg salary' from sampel_university.instructor i group by i.dept_name;
+
+select i.name, min(i.salary) from sampel_university.instructor i;
+
+select c.title, max(c.credits) "max cred" from sampel_university.course c;
