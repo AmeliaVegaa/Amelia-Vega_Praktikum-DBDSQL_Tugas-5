01c99f5f57c6b9781f7d4ecaaacb728e8c0ca071
AmeliaVegaa <133181467+AmeliaVegaa@users.noreply.github.com>
2023-11-26 00:11:15 +0700

    Update code.sql

--- code.sql
+++ code.sql
@@ -1,5 +1,5 @@
-select concat(' ',dept_name, building) as PROFIL
-from sampel_university.department;
+select concat(ID,name) as profil
+from sampel_university.student;
 
 select substring_index(dept_name,' ',1) as DEPTNAME_INDEX
 from sampel_university.department;
@@ -7,5 +7,8 @@ from sampel_university.department;
 select substr(dept_name,1,3) as KARAKTER
 from sampel_university.department;
 
-select title,length(title) as TOTAL_CHAR
+select title,length (title) as TOTAL_CHAR
 from sampel_university.course;
+
+select name, replace (name,'JONO','BUDI') as NAMA_BARU
+from akademik.mahasiswa ;
